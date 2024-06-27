80a2d34398887a39020cc892e1721db38a1cfb2c
Andrewdark <samsonov.a@ukr.net>
2024-06-27 17:44:21 +0300

    index.tsx to moder style

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import {App} from './components/App';
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
+import { createRoot } from "react-dom/client";
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
